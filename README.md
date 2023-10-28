7d0fdbfcad00a6a6b4e8211a83f5a28cab40228a
bryantwilliam <gogobebe2@gmail.com>
2023-10-28 17:27:25 +1300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# point_of_sale_design
+
+A clone of https://dribbble.com/shots/14139155-Point-of-Sale-System-Design
+
+Practicing UI implementation
---
+++ android/app/src/main/kotlin/com/example/point_of_sale_design/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.point_of_sale_design
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
